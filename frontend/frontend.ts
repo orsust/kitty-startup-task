4a870ed4bdf6350395000e8037d57f61afd5e772
orsust <a.ustinov@g.nsu.ru>
2025-11-20 01:51:45 +0700

    Update frontend.ts

--- frontend/frontend.ts
+++ frontend/frontend.ts
@@ -2,19 +2,24 @@ import * as express from "express";
 import * as path from "path";
 
 const app = express();
-app.set('views', path.join(__dirname, '/'));
+const port: number = 2000;
+const backendUrl = "http://backend:3601";
+
+// Настраиваем шаблонизатор
+app.set('views', path.join(__dirname));
 app.set('view engine', 'jade');
 
-const port: number = 3000;
-const backendUrl = "http://nginx/api";
-
 app.get('/', async (req, res) => {
-    const cat_data = await fetch(`${backendUrl}/cat_get`);
-    const data = await cat_data.text();
-
-    res.render('index.jade', { title: "KITTY Frontend", cat_url: data });
+    try {
+        const cat_data = await fetch(`${backendUrl}/cat_get`);
+        const data = await cat_data.text();
+        res.render('index.jade', { title: "KITTY frontend", cat_url: data });
+    } catch (error) {
+        console.error('Error fetching cat:', error);
+        res.render('index.jade', { title: "KITTY frontend", cat_url: '' });
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Frontend app listening on port ${port}`)
-})
+    console.log(`Frontend app listening on port ${port}`);
+});
