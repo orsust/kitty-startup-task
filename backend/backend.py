bfc79e67aae186dd1689a0455c425ba9cea8b95a
orsust <a.ustinov@g.nsu.ru>
2025-11-20 01:56:08 +0700

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -15,4 +15,4 @@ def hello_world():
         return ''
 
 if __name__ == '__main__':
-    app.run(debug=True, host='::', port='3001')
+    app.run(debug=True, host='0.0.0.0', port=3601)  # убрал кавычки у порта
